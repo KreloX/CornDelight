33afe93782bed8c6dfd149efd9120e65ae22329a
KreloX <krelox@seznam.cz>
2023-08-11 14:42:26 +0200

    Port language files from the original mod

--- src/main/java/krelox/corndelight/data/CornDelightLang.java
+++ src/main/java/krelox/corndelight/data/CornDelightLang.java
@@ -15,6 +15,7 @@ public class CornDelightLang extends FabricLanguageProvider {
     public void generateTranslations(TranslationBuilder translationBuilder) {
         translationBuilder.add(CornDelightBlocks.CORN_CROP, "Corn");
         translationBuilder.add("itemgroup.corn_delight", "Corn Delight");
+        translationBuilder.add("corndelight.block.popcorn.barehand", "Use your bare hand to eat popcorn.");
         CornDelightItems.items.forEach(item -> translationBuilder.add(item, formatName(item.toString())));
     }
 
